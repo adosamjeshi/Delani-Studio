623bf19d026580a8e7297c69b5903ede2f117fe3
Adan Muktar <adosamjeshi@gmail.com>
2020-02-25 09:40:12 +0300

    final changes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,62 @@
-$(document).ready(function(){
-
-    $(".whatwedo").hide();
-    $("#design").click(function(){
-        $("#design p").toggle();
-        $("#design img").toggle();
-
+$(document).ready(function () {
+  $("#design").click(function () {
+    $("#caution1").toggle();
+    $("#design").toggle();
+  });
 });
-})
+$(document).ready(function () {
+  $("#development").click(function () {
+    $("#caution2").toggle();
+    $("#development").toggle();
+
+  });
+});
+
+$(document).ready(function () {
+  $("#management").click(function () {
+    $("#caution3").toggle();
+    $("#management").toggle();
+
+  });
+});
+
+$(document).ready(function () {
+  $("#DEVELOPMENT2").click(function () {
+    $("#DEVELOPMENT").toggle();
+    $("#DEVELOPMENT2").toggle();
+
+  });
+});
+
+$(document).ready(function () {
+  $("#PRODUCT").click(function () {
+    $("#PRODUCT").toggle();
+    $("#PRODUCT2").toggle();
+
+  });
+});
+
+$(document).ready(function () {
+  $("#PRODUCT2").click(function () {
+    $("#PRODUCT").toggle();
+    $("#PRODUCT2").toggle();
+
+  });
+});
+
+$(document).ready(function () {
+  $(".form2").submit(function (event) {
+    var name = $(".name").val();
+    alert("How are you " + name + ". Thank you for contacting us.");
+    event.preventDefault();
+  });
+});
+function myFunction() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  if (name == "" || email == "" || message == "") {
+    alert("enter your name, email and a commment")
+  }
+  else alert(name + " ,we have received your message. Thank you for reaching to us.");
+}
